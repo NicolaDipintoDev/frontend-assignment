71e662130df2904e5f0f4949f2413d80d8b89ddf
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-10 15:14:23 +0200

    add all types

--- packages/client/src/components/ByTypeSearch.tsx
+++ packages/client/src/components/ByTypeSearch.tsx
@@ -10,16 +10,33 @@ const ByTypeSearch = ({ query, setQuery }: Props) => {
     const onClick = (value: string) => {
         setQuery(value);
     }
+    const types = [
+        'Normal',
+        'Fire',
+        'Grass',
+        'Water',
+        'Poison',
+        "Electric",
+        "Fighting",
+        "Fairy",
+        "Ice",
+        "Flying",
+        "Psychic",
+        "Bug",
+        "Steel",
+        "Ground",
+        "Rock",
+        "Ghost",
+        "Dragon"];
     return <div className="searchBar">
         Choose type: {' '}
         <Radio.Group
             defaultValue={'Normal'}
             buttonStyle="solid"
             onChange={e => onClick(e.target.value)}>
-            {['Normal', 'Fire', 'Grass', 'Water'].map(type =>
+            {types.map(type =>
 
                 <Radio.Button key={type} value={type}>{type}</Radio.Button>
-
 
             )}
         </Radio.Group>
