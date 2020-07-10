832e8d55af98b5d3e9a728ae37bdd30e178bfc01
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-10 16:48:05 +0200

    finish easter egg

--- packages/client/src/pages/BestStarterEver.tsx
+++ packages/client/src/pages/BestStarterEver.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { Radio, Alert } from 'antd';
+import { Radio, Alert, Typography } from 'antd';
 import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
 
 
 const BestStarteEver = () => {
@@ -11,26 +13,36 @@ const BestStarteEver = () => {
 
     const result = () => {
         let message: string;
+        let correct: boolean;
         switch (answer) {
             case 'Bulbasaur':
                 message = 'Are you Kidding me?';
+                correct = false;
                 break;
             case 'Charmander':
                 message = 'The most popular is not always the best';
+                correct = false;
                 break;
             case 'Squirtle':
                 message = 'You are a great Pokemon trainer!!!';
+                correct = true;
+                break;
+            default:
+                message = 'starter name not valid';
+                correct = false;
                 break;
 
         }
+        return <Alert message={message} type={correct ? 'success' : 'error'} />
     }
+
     return <div className="App">
         <p className='link'>
             <Link to="/">
                 Back to Pokemons page
             </Link>
         </p>
-        Choose type: {' '}
+        Choose wisely: {' '}
         <Radio.Group
             defaultValue={'Normal'}
             buttonStyle="solid"
@@ -42,7 +54,11 @@ const BestStarteEver = () => {
             )}
         </Radio.Group>
 
-        {answer !== '' && <div>{answer}</div>}
+        {answer !== '' &&
+            <div className="Alert">
+                <Title>Professor Oak say:</Title>
+                {result()}
+            </div>}
     </div>
 }
 
