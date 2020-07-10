7da17de8ac82d18e7f48a4ecae02ee4dadf9c622
NicolaDipintoDev <dipinto.nicola.dev@gmail.com>
2020-07-10 16:00:09 +0200

    add types react router

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,63 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { Typography } from 'antd';
-import 'antd/dist/antd.css';
-import SearchType from './components/SearchType';
-import SearchBar from './components/searchMethods/ByNameSearch';
-import ByTypeSearch from './components/searchMethods/ByTypeSearch'
-import GetSearchMethod from './components/searchMethods/GetSearchMethod'
-import PokemonsQuery from './api/PokemonsQuery';
+import Pokemons from './pages/Pokemons';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const { Title } = Typography;
+
 
 
 function App() {
-
-  const client = new ApolloClient({
-    uri: 'http://localhost:4000',
-  });
-
-  const [searchType, setSearchType] = useState<string>('byName');
-  const [query, setQuery] = useState<string>('');
-  const [after, setAfter] = useState<string>('');
-  const [prevAfter, setPrevAfter] = useState<string>('000');
-
-
-  return (
-    <ApolloProvider client={client}>
-      <div className="App">
-
-        <Title>
-          Pokemon
-          </Title>
-
-        <SearchType
-          searchType={searchType}
-          setSearchType={setSearchType}
-          setQuery={setQuery}
-        />
-
-        <GetSearchMethod
-          method={searchType}
-          query={query}
-          setQuery={setQuery}
-        />
-
-        <PokemonsQuery
-          searchedValue={query}
-          searchType={searchType}
-          setQuery={setQuery}
-          after={after}
-          setAfter={setAfter}
-          prevAfter={prevAfter}
-          setPrevAfter={setPrevAfter}
-        />
-
-      </div>
-    </ApolloProvider>
-  );
+  return <Router>
+    <div>
+      <Route exact path="/" component={Pokemons} />
+    </div>
+  </Router>
 }
 
 export default App;
